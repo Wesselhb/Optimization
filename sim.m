082f1f9aa5184eb9faa004495b670a8abe5c7f5f
Wesselhb <wesselhogenboom@gmail.com>
2024-10-20 15:59:23 +0200

    Update sim.m

--- sim.m
+++ sim.m
@@ -1,6 +1,7 @@
 % Student dependent variables
 Da = [4 6 5];
 Db = [1 8 8];
+global E2 E3
 E1 = Da(1) + Db(1);
 E2 = Da(2) + Db(2);
 E3 = Da(3) + Db(3);
@@ -20,71 +21,33 @@ T = 10/3600; % [hr]
 L = 1; % [km]
 LAMBDA = 3; % [lanes]
 Dr = 1500;
+%% Q4
+z=ones(2,120);
+z(1,:)=80*z(1,:);
+lb = 60*ones(2,120);
+lb(2,:) = 1*lb(1,:);
+ub = ones(2,120);
+ub(1,:) = 120*ub(1,:);
 
-% functions
-function [qi, rhonext] = nextrho(rhoi, qprev, qr, vi)
-    global LAMBDA T L RHOc;
-    qi = LAMBDA*rhoi*vi;
-    rhonext = min([rhoi + (T/(LAMBDA*L))*(qprev - qi + qr) RHOc]);
-end
-
-
-function vnext = nextv(vi, vprev, rhonext, rhoi, VSL)
-    global ALPHA Vf A RHOc T TAU L MU K;
-    Vi = min([(1+ALPHA)*VSL; Vf*exp((-1/A)*(rhoi/RHOc)^A)]);
-    posterm = vi + (T/TAU)*(Vi - vi) + (T/L)*vi*(vprev - vi);
-    negterm = ((MU * T * (rhonext - rhoi))/(TAU * L * (rhoi + K)));
-    vnext = posterm - negterm;
-end
-
-% Simulation
-rho = zeros(5, 120);
-v = zeros(5, 120);
-omega = zeros(1,120);
-rho(:,1) = 30 * ones(5,1);
-v(:,1) = 80 * ones(5,1);
-
-for k=1:120
-    if k < 60
-        q0 = 7000 + 100*E2;
-    else
-        q0 = 2000 + 100*E3;
-    end
-
-    [qi, rho(1,k+1)] = nextrho(rho(1,k), q0, 0, v(1, k));
-    v(1, k+1) = nextv(v(1, k), v(1,k), rho(1+1, k), rho(1, k), 120);
-
-    for i=2:3
-        [qi, rho(i,k+1)] = nextrho(rho(i,k), qi, 0, v(i, k));
-        v(i, k+1) = nextv(v(i, k), v(i-1,k), rho(i+1, k), rho(i, k), 120);
-    end
-    rx = 1;
-    qr = min([rx*Cr; Dr+omega(k)/T; Cr*(RHOm - rho(4,k)/(RHOm-RHOc))]);
-    omega(k+1) = omega(k) + T*(Dr - qr);
-    [qi, rho(4, k+1)] = nextrho(rho(4,k), qi, qr, v(4, k));
-    v(4, k+1) = nextv(v(4, k), v(3,k), rho(5,k), rho(4, k), 120);
-    [~, rho(5,k+1)] = nextrho(rho(5,k), qi, 0, v(5, k));
-    v(5, k+1) = nextv(v(5, k), v(5-1,k), rho(5, k), rho(5,k), 120);
-    end
-
-%plotting
-figure()
-title('Velocity')
-xlabel('Time')
-ylabel('Velocity')
-hold on
-for i=1:5
-    plot(v(i,:))
-end
-legend('Segment 1', 'Segment 2', 'Segment 3', 'Segment 4', 'Segment 5')
+VSL_min = fmincon(@costfunc2,z,[],[],[],[],lb,ub);
 
 
-figure()
-title('density')
-xlabel('Time')
-ylabel('density')
-hold on
-for i=1:5
-    plot(rho(i,:))
-end
-legend('Segment 1', 'Segment 2', 'Segment 3', 'Segment 4', 'Segment 5')
+%% Q5
+z=ones(2,120);
+z(1,:)=80*z(1,:);
+lb = 60*ones(2,120);
+lb(2,:) = 0*lb(1,:);
+ub = ones(2,120);
+ub(1,:) = 120*ub(1,:);
+
+Xbar = fmincon(@costfunc2,z,[],[],[],[],lb,ub);
+
+%% Q8
+z=ones(2,120);
+z(1,:)=80*z(1,:);
+lb = 60*ones(2,120);
+lb(2,:) = 0*lb(1,:);
+ub = ones(2,120);
+ub(1,:) = 120*ub(1,:);
+
+Hbar = ga(@costfunc2,240,[],[],[],[],lb,ub);
