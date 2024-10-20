92df61a450ecb85865dc140b556d5639ef3010cb
Daan <dagvanhaasteren@gmail.com>
2024-10-20 14:09:35 +0200

    added cost and optimization files
    also updated simulation file

--- sim.m
+++ sim.m
@@ -6,22 +6,23 @@ E2 = Da(2) + Db(2);
 E3 = Da(3) + Db(3);
 
 % Constants
-global TAU MU Cr RHOm ALPHA K A Vf RHOc T L LAMBDA;
-TAU = 10;
-MU = 80;
-Cr = 2000;
-RHOm = 120;
-ALPHA = 0.1;
-K = 10;
-A = 2;
-Vf = 110;
-RHOc = 33.5 + E1/3;
-T = 10;
-L = 10000;
-LAMBDA = 3;
+global TAU MU Cr RHOm ALPHA K A Vf RHOc T L LAMBDA Dr;
+TAU = 10/3600; % [hr]
+MU = 80; % [km^2/hr]
+Cr = 2000; % [veh/hr]
+RHOm = 120; % [veh/hr/lane]
+ALPHA = 0.1; % []
+K = 10; % [veh/lane/km]
+A = 2; % []
+Vf = 110; % [km/hr]
+RHOc = 33.5 + E1/3; % [veh/km/lane]
+T = 10/3600; % [hr]
+L = 1; % [km]
+LAMBDA = 3; % [lanes]
+Dr = 1500;
 
 % functions
-function [qi rhonext] = nextrho(rhoi, qprev, qr, vi)
+function [qi, rhonext] = nextrho(rhoi, qprev, qr, vi)
     global LAMBDA T L RHOc;
     qi = LAMBDA*rhoi*vi;
     rhonext = min([rhoi + (T/(LAMBDA*L))*(qprev - qi + qr) RHOc]);
@@ -39,6 +40,7 @@ end
 % Simulation
 rho = zeros(5, 120);
 v = zeros(5, 120);
+omega = zeros(1,120);
 rho(:,1) = 30 * ones(5,1);
 v(:,1) = 80 * ones(5,1);
 
@@ -48,18 +50,22 @@ for k=1:120
     else
         q0 = 2000 + 100*E3;
     end
-    
+
     [qi, rho(1,k+1)] = nextrho(rho(1,k), q0, 0, v(1, k));
     v(1, k+1) = nextv(v(1, k), v(1,k), rho(1+1, k), rho(1, k), 120);
 
-    for i=2:4
+    for i=2:3
         [qi, rho(i,k+1)] = nextrho(rho(i,k), qi, 0, v(i, k));
         v(i, k+1) = nextv(v(i, k), v(i-1,k), rho(i+1, k), rho(i, k), 120);
     end
-
-    [qi, rho(5,k+1)] = nextrho(rho(5,k), qi, 0, v(i, k));
+    rx = 1;
+    qr = min([rx*Cr; Dr+omega(k)/T; Cr*(RHOm - rho(4,k)/(RHOm-RHOc))]);
+    omega(k+1) = omega(k) + T*(Dr - qr);
+    [qi, rho(4, k+1)] = nextrho(rho(4,k), qi, qr, v(4, k));
+    v(4, k+1) = nextv(v(4, k), v(3,k), rho(5,k), rho(4, k), 120);
+    [~, rho(5,k+1)] = nextrho(rho(5,k), qi, 0, v(5, k));
     v(5, k+1) = nextv(v(5, k), v(5-1,k), rho(5, k), rho(5,k), 120);
-end
+    end
 
 %plotting
 figure()
